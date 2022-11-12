54b24a0b7c51169512eac500072c8f7f51a1c200
Igor Gregoire <54171035+igivotron@users.noreply.github.com>
2022-11-12 18:14:05 +0100

    Update ex_simu.py
    - Rajoute d'un paramètre de décallage

--- exemple_tracker/ex_simu.py
+++ exemple_tracker/ex_simu.py
@@ -4,10 +4,9 @@ def force_frot_fun(v,k):
     f_fr = k*v
     return f_fr
 
-def main(x, v, ligma, k, dt, m):
+def main(x, v, ligma, k, dt, m,decal):
     v_t = [v]
     x_t = [x]
-    start = 0
     t = 0
     loop = True
     while loop:
@@ -20,6 +19,6 @@ def main(x, v, ligma, k, dt, m):
             loop = False
         t+=1
     t =t*dt
-    tt = np.arange(0,t+dt,dt)
+    tt = np.arange(decal,decal+t+dt,dt)
+    print(tt)
     return v_t, x_t, tt
-
