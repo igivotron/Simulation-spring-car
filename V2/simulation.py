d7caed32277c7425e584cb6d9bf6770523c73e7c
Igor Gregoire <54171035+igivotron@users.noreply.github.com>
2022-12-14 12:27:39 +0100

    Update simulation.py

--- V2/simulation.py
+++ V2/simulation.py
@@ -1,53 +1,89 @@
 import matplotlib.pyplot as plt
-import numpy as np
+
 
 class Engin:
-    def __init__(self,f,k):
-        self.k = 865.6*4
-        self.ratio = 2/5
-        self.f_eng = f
-        self.f_air = k
+    def __init__(self):
+        self.k = 865.6 * 4
+        self.ratio = 2 / 5
+        self.f_eng = 1
+        self.f_air = 1
+        self.dx_res = pow(10 / self.k, 1 / 2)
+        self.n = 5
+        self.m = 0.3
 
     def frot_engr(self, v):
-        return -self.f_eng*v
+        return -self.f_eng * v
 
-    def frot_air(self,v):
-        return -self.f_air*v
+    def frot_air(self, v):
+        return -self.f_air * v
 
-    def joules(self):
-        return pow(10/self.k,1/2)
+    def relachement_ressort(self, r):
+        self.dx_res -= r
 
-    def force_mot(self):
-        
+    def force_ressort(self):
+        return self.k * self.dx_res
+
+    def ratio(self):
+        return pow(self.ratio, self.n)
+
+    def acceleration(self, v):
+        return (((self.force_ressort() - self.frot_engr(v)) / self.ratio) - self.frot_air(v)) / self.m
 
 
 class Simu:
-    def __init__(self,dt,sigma):
+    def __init__(self, engin, dt, sigma):
         self.dt = dt
         self.g = 9.81
         self.x = 0
         self.v = 0
-        self.sigma =sigma
+        self.sigma = sigma
         self.loop = True
+        self.engin = engin
 
-    def test_loop(self,v):
-        if v < self.sigma:
+        self.l_x = [0]
+        self.l_v = [0]
+
+    def test_loop(self):
+        if self.engin.dx_res < self.sigma:
             self.loop = False
 
+    def calc1(self):
+        v = 0
+        x = 0
+        while self.loop:
+            v += self.engin.acceleration(v) * self.dt
+            x += v * self.dt
+            self.l_v.append(v)
+            self.l_x.append(x)
+            self.engin.relachement_ressort(x)
+            self.test_loop()
+
+        while v > self.sigma:
+            v += self.engin.frot_air(v) * self.dt
+            x += v * self.dt
+            self.l_v.append(v)
+            self.l_x.append(x)
+
+        return (self.l_x, self.l_v)
+
     def lst_x(self):
-        while loop:
+        a = self.calc1()
+        return a[0]
+
+    def lst_v(self):
+        a = self.calc1()
+        return a[1]
 
 
 class Graph:
-    def __init__(self,t,l_x,l_v,n_x="Temps",n_y="Position"):
+    def __init__(self, t, simu, n_x="Temps", n_y="Position"):
         self.title = t
         self.n_x = n_x
         self.n_y = n_y
-        self.l_x = l_x
-        self.l_v = l_v
+        self.simu = simu
 
-    def title(self):
-        plt.title(self.title())
+    def f_title(self):
+        plt.title(self.title)
 
     def legend(self):
         plt.xlabel(self.n_x)
@@ -55,31 +91,25 @@ class Graph:
         plt.legend()
 
     def plot_x(self):
-        plt.plot(self.l_x,label="position en [m]")
-        self.title()
+        plt.plot(self.simu.lst_x(), label="position en [m]")
+        self.f_title()
         self.legend()
         plt.show()
 
     def plot_v(self):
-        plt.plot(self.l_v,label="vitesse en [m/s]")
-        self.title()
+        plt.plot(self.simu.lst_v(), label="vitesse en [m/s]")
+        self.f_title()
         self.legend()
         plt.show()
 
     def plot_both(self):
-        plt.plot(self.l_x,label="position en [m]")
-        plt.plot(self.l_v,label="vitesse en [m/s]")
-        self.title()
+        plt.plot(self.simu.lst_x(), label="position en [m]")
+        plt.plot(self.simu.lst_v(), label="vitesse en [m/s]")
+        self.f_title()
         self.legend()
         plt.show()
 
-
-
-
-
-
-
-
-
-
-
+voiture = Engin()
+simulation = Simu(voiture,0.001,0.0001)
+graphique = Graph("Simulation", simulation)
+graphique.plot_both()
